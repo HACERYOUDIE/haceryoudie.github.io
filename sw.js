9e78bd9f53953cf48ae88191750b6122393136aa
HACERYOUDIE <152202395+HACERYOUDIE@users.noreply.github.com>
2026-03-08 09:48:04 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,11 @@
 self.addEventListener('notificationclick', (event) => {
     event.notification.close();
-    var chatUrl = `/chat?id=${event.notification.data.chat_id}`;
-    
     event.waitUntil(
         clients.matchAll({ type: 'window', includeUncontrolled: true }).then(windowClients => {
-            for (var i = 0; i < windowClients.length; i++) {
-                var client = windowClients[i];
-                if (client.url.includes('/chat') && 'focus' in client) {
-                    return client.focus();
-                }
-            }
-            if (clients.openWindow) {
-                return clients.openWindow(chatUrl);
-            }
+            if (windowClients.length > 0) {
+                windowClients[0].focus();
+            } 
         })
     );
+
 });
