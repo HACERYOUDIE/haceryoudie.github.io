c3c72e2409dc7c217fba308dd25dcd9a4a3b44a9
HACERYOUDIE <152202395+HACERYOUDIE@users.noreply.github.com>
2026-03-08 10:07:44 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('notificationclick', (event) => {
+    event.notification.close();
+    event.waitUntil(
+        clients.matchAll({ type: 'window', includeUncontrolled: true }).then(windowClients => {
+            if (windowClients.length > 0) {
+                windowClients[0].focus();
+            } 
+        })
+    );
+
+});
